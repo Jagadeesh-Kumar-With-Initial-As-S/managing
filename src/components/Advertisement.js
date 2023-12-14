d6afa2646b2d9a67834f472c4216656b827a1c1d
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-12-14 09:52:15 +0530

    commit on terminal

---
+++ src/components/Advertisement.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Advertisement.css";
+function Advertisement() {
+  return (
+    <div className="Advertisement">
+      Submitted and coded by Jagadeesh Kumar with initial as S, you may send
+      mail to my email address which is equal to jagadeesh_2k17@proton.me and
+      you may contribute some money to my Indian Unified Payment Interface (UPI)
+      which is equal to jagadeesh-kumar@airtel .
+    </div>
+  );
+}
+
+export default Advertisement;
